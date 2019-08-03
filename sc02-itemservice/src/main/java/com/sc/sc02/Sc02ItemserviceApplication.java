d7cdb318ed1243425c95761d210809bedeb2bf45
93LifeAfterLife <tangdingjnust@163.com>
2019-08-03 17:08:45 +0800

    2019年8月3日17:08:03

--- sc02-itemservice/src/main/java/com/sc/sc02/Sc02ItemserviceApplication.java
+++ sc02-itemservice/src/main/java/com/sc/sc02/Sc02ItemserviceApplication.java
@@ -2,10 +2,11 @@ package com.sc.sc02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class Sc02ItemserviceApplication {
 
     public static void main(String[] args) {
--- sc05-eureka/src/main/java/com/sc/sc05/Sc05EurekaApplication.java
+++ sc05-eureka/src/main/java/com/sc/sc05/Sc05EurekaApplication.java
@@ -2,6 +2,7 @@ package com.sc.sc05;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EurekaClientConfigBean;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
